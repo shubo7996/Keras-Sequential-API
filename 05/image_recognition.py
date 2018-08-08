2890df199a0c3b030741ee71f0d56b3aa53eac1b
shubo7996 <whatsupmofo420@gmail.com>
2018-08-08 09:41:13 +0530

    using_resnet50_model

--- 05/image_recognition.py
+++ 05/image_recognition.py
@@ -3,25 +3,25 @@ from keras.preprocessing import image
 from keras.applications import resnet50
 
 # Load Keras' ResNet50 model that was pre-trained against the ImageNet database
-model =
+model = resnet50.ResNet(50)
 
 # Load the image file, resizing it to 224x224 pixels (required by this model)
-img =
+img = image.load_img('bay.jpg', target_size=(224,224))
 
 # Convert the image to a numpy array
-x =
+x = image.img_to_array(img)
 
 # Add a forth dimension since Keras expects a list of images
-x =
+x = np.expand_dims(x, axis=0)
 
 # Scale the input image to the range used in the trained network
-x =
+x = resnet50.preprocess_input(x)
 
 # Run the image through the deep neural network to make a prediction
-predictions =
+predictions = models.predict(x)
 
 # Look up the names of the predicted classes. Index zero is the results for the first image.
-predicted_classes =
+predicted_classes = resnet50.decode_predictions(predictions, top=9)
 
 print("This is an image of:")
 
